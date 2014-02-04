e8e53292eb8cda89ceb4f1d106ff55bfea8a8fb5
Joseph Dykstra <josephdykstra@gmail.com>
2014-02-04 15:43:59 -0600

    Initial commit

---
+++ main.js
@@ -0,0 +1,24 @@
+﻿var stripe = require("stripe")(INSERT_API_KEY_HERE)
+
+//INerrFunc needs args [err]
+function chargeCreate(INmoneyInCents, INcard, INerrFunc) {
+	var temp = stripe.charges.create({
+		amount: INmoneyInCents,
+		currency: "usd",
+		card: INcard,
+		description: "Buying 3D-printed objects"
+	})
+	if (typeof temp !== "object")
+		INerrFunc(temp)
+	else
+	
+}
+
+//INfunc needs args [err], [charge]
+function chargeCapture(INid, INfunc) {
+	stripe.charges.capture(INid, INfunc)
+}
+
+function chargeDetails(INid) {
+	return stripe.charges.retrieve({id: INid})
+}
---
+++ test/testCharging.js
@@ -0,0 +1,10 @@
+﻿var sensitiveInfo = require("../../admesh-and-stripe_sensitive-info/sensitiveInfo")
+var sampleApiKey =	sensitiveInfo[0]
+var sampleCard =	sensitiveInfo[1]
+//require("./main.js")
+
+var test = require('tap').test
+
+test("blah blah blah", function test(t) {
+	
+}
