cfc3d8fa9535abe6b28c621d295df16771e80af8
Joseph Dykstra <josephdykstra@gmail.com>
2014-02-05 14:39:09 -0600

    Added a unit test that does tests nothing

--- test/testCharging.js
+++ test/testCharging.js
@@ -1,10 +1,14 @@
-﻿var sensitiveInfo = require("../../admesh-and-stripe_sensitive-info/sensitiveInfo")
-var sampleApiKey =	sensitiveInfo[0]
-var sampleCard =	sensitiveInfo[1]
-//require("./main.js")
+﻿var secret = require("../../admesh-and-stripe_sensitive-info/sensitiveInfo.js")
+var testCard = {
+	number: "4242424242424242", // Fake number, too bad for you. ;)
+	exp_month: 12,
+	exp_year: 2014
+}
+var Main = require("../main.js")
 
 var test = require('tap').test
 
 test("blah blah blah", function test(t) {
-	
-}
+	t.equal(1,1) //tests NOTHING!!!
+	t.end()
+})
