811862435c97084dd451ffa15e2c0249ad9d17a0
Joseph Dykstra <josephdykstra@gmail.com>
2014-02-05 14:38:43 -0600

    Created an example for the main file

---
+++ example.js
@@ -0,0 +1,13 @@
+﻿var secret = require("../admesh-and-stripe_sensitive-info/sensitiveInfo.js")
+var testCard = {
+	number: "4242424242424242", // Fake number, too bad for you. ;)
+	exp_month: 12,
+	exp_year: 2014
+}
+var Main = require("./main.js")
+
+var stripeTransfer = new Main(secret.apiKey)
+//console.dir(stripeTransfer)
+var success = stripeTransfer.chargeCreate(500, "usd", testCard)
+
+console.log("oh yes")
