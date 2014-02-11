3f05d1151ed798f95899dfd439030b0360c1cc71
Joseph Dykstra <josephdykstra@gmail.com>
2014-02-11 15:04:12 -0600

    main func now takes CBs, example updated with CBs

--- stripe-example.js
+++ stripe-example.js
@@ -1,16 +1,32 @@
-﻿var secret = require("../stripe-and-dwolla_sensitive-info/stripe.js")
+﻿var secret = require("..\\stripe-and-dwolla_sensitive-info/stripe.js")
 var testCard = {
 	number: "4242424242424242", // Fake number, too bad for you. ;)
 	exp_month: 12,
 	exp_year: 2014
 }
-var Main = require("./main.js")
+var StripeWrapper = require("./stripe-main.js")
+var stripeTransfer = new StripeWrapper(secret.apiKey)
 
-var stripeTransfer = new Main(secret.apiKey)
-//console.dir(stripeTransfer)
-var success = stripeTransfer.chargeCreate(500, "usd", testCard)
-if (success) {
-	//um
-} else { //error
-	console.log("Error: "+err+"\n" + err.message);
+var cbDetailsGood = function(obj) { console.log("Retrieval Success: "+obj) }
+var cbDetailsError = function(err)  { console.log("Retrieval Error: "+err.message) }
+
+var cbChargeGood = function(id) {
+	console.log("Successful Charge ID: "+id)
+	stripeTransfer.details(id, cbDetailsGood, cbDetailsError)
 }
+var cbChargeError = function(err) {
+	console.log("Charge Error: "+err.message)
+	stripeTransfer.details(id, cbDetailsGood, cbDetailsError)
+	//Yes, it looks up the details of a failed charge.
+}
+var cbChargeNeutral = function(suc, id) {
+	console.log("Charge Success: "+suc+"\nCharge ID: "+id)
+	stripeTransfer.details(id, cbDetailsGood, cbDetailsError)
+}
+
+
+//Asynchronous
+stripeTransfer.charge("", 50, {card:testCard}, cbChargeGood, cbChargeError) //Good
+stripeTransfer.charge("", 20, {card:testCard}, cbChargeGood, cbChargeError) //Error
+stripeTransfer.charge("", 50, {card:testCard}, cbChargeNeutral) //Good
+stripeTransfer.charge("", 20, {card:testCard}, cbChargeNeutral) //Error
--- stripe-main.js
+++ stripe-main.js
@@ -4,20 +4,39 @@
 //Constructor
 module.exports = function(apiKey) {
 	var stripe = require("stripe")(apiKey)
-	
-	this.chargeCreate = function(moneyInCents, currency, card) {
-		return stripe.charges.create({
-			amount: moneyInCents,
-			currency: currency,
-			card: card
-		})
-	}
+	return {
+		charge: function charge(desc, amount, object, cbGood, cbErr) { //takes 4 or 5 args
+			object.description = desc
+			object.currency = "usd"
+			object.amount = amount
+			if (typeof cbErr === "function") {		//TEST MODE (append cbErr)
+				stripe.charges.create(object).then(
+					function(goodObj) { cbGood(goodObj.id) },
+					cbErr
+				)
+			} else {								//REAL MODE (omit cbErr)
+				stripe.charges.create(object).then(
+					function (goodObj) { cbGood(true,  goodObj.id) },
+					function (err)     { cbGood(false, "") }
+				)
+			}
+		}, // cbErr(err)   cbGood(uniqueId)
 
-	this.chargeCapture = function(INid, INfunc) {
-		stripe.charges.capture(INid, INfunc)
-	} //INfunc needs args [err], [charge]
-
-	this.chargeDetails = function(INid) {
-		return stripe.charges.retrieve({id: INid})
+		details: function details(INid, cbGood, cbError) { //must take all 3
+			if (typeof INid !== 'undefined' && INid !== "") {
+				stripe.charges.retrieve(INid).then(
+					cbGood,
+					cbError
+				)
+			} else {
+				cbError({
+					type: 'StripeInvalidRequest_details',
+					param: 'INid',
+					message: 'ID must be valid'
+				})
+			}
+		},
+		
+		id: "stripe"
 	}
 }
